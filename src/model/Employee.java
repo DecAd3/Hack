2bee1b430422a6c37f8f2828a271c74f11d904a5
DUAN Longteng <longtduan2@ad.cityu.edu.hk>
2021-01-30 14:40:01 +0800

    Emp and room

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -1,5 +1,47 @@
 package model;
 
 public class Employee {
-    
+    private String number;
+    private int workStatus;
+    private int healthyStatus;
+
+    public Employee(Sting number) {
+        this.number = number;
+        this.workStatus = STATUS_HOMING;
+        this.healthyStatus = STATUS_HEALTHY;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public int getWorkStatus() {
+        return workStatus;
+    }
+
+    public int getHealthyStatus() {
+        return healthyStatus;
+    }
+
+    public void setWorkStatus(int workStatus) {
+        this.workStatus = workStatus;
+    }
+
+    public void setHealthyStatus(int healthyStatus) {
+        this.healthyStatus = healthyStatus;
+    }
+
+    // work status
+    public final static int STATUS_HOMING = 0;
+    public final static int STATUS_WORKING = 1;
+    public final static int STATUS_RESTING = 2;
+    public final static int STATUS_MEETING = 3;
+    public final static int STATUS_PEEING = 4;
+    public final static int STATUS_LIFTING = 5;
+    // healthy status
+    public final static int STATUS_HEALTHY = 0;
+    public final static int STATUS_RISKY = 1;
+    public final static int STATUS_INFECTED = 2;
+    public final static int STATUS_ISOLATED = 3;
+
 }
