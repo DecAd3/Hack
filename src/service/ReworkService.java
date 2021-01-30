3a89ba63019bbb89afa354e742de2306d98de8a9
WANG Enrui <enruiwang2@ad.cityu.edu.hk>
2021-01-30 16:45:52 +0800

    finish rework

--- src/service/ReworkService.java
+++ src/service/ReworkService.java
@@ -1,19 +1,37 @@
 package service;
 
+import static util.Constants.FIND_OUT_POSSIBILITY;
+import static util.Constants.LIFTING_POSSIBILITY;
+import static util.Constants.LIFT_DURATION;
 import static util.Constants.LOG_PATH;
+import static util.Constants.MEETING_DURATION;
+import static util.Constants.MEETING_POSSIBILITY;
+import static util.Constants.PEEING_POSSIBILITY;
+import static util.Constants.PEE_DURATION;
+import static util.Constants.RESTING_POSSIBILITY;
+import static util.Constants.REST_DURATION;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 import model.Company;
 import model.Employee;
 import model.Employee.HEALTHY_STATUS;
 import model.Employee.WORK_STATUS;
+import model.Floor;
+import model.RoomModel.Elevator;
+import model.RoomModel.MeetingRoom;
+import model.RoomModel.RestRoom;
+import model.RoomModel.Room;
+import model.RoomModel.Room.STATUS;
+import model.RoomModel.Toilet;
 
 public class ReworkService {
     private static ReworkService _instance = new ReworkService();
@@ -40,10 +58,10 @@ public class ReworkService {
     }
 
     public void info() throws IOException {
-        int OFFICEING_AMOUNT = company.getEmployeePool().stream()
+        int OFFICE_AMOUNT = company.getEmployeePool().stream()
                 .filter(employee -> employee.getWorkStatus() == WORK_STATUS.WORKING).collect(Collectors.toList())
                 .size();
-        int MEETTING_AMOUNT = company.getEmployeePool().stream()
+        int MEETING_AMOUNT = company.getEmployeePool().stream()
                 .filter(employee -> employee.getWorkStatus() == WORK_STATUS.MEETING).collect(Collectors.toList())
                 .size();
         int RESTING_AMOUNT = company.getEmployeePool().stream()
@@ -59,48 +77,208 @@ public class ReworkService {
                 .size();
         logFile.write("---------");
         logFile.write("\n");
-        logFile.write("世界时间：" + WORLD_TIME);
+        logFile.write("World Time：" + WORLD_TIME);
         logFile.write("\n");
-        logFile.write("复工第" + (WORLD_TIME / 48) + "天");
+        logFile.write("DAY " + (WORLD_TIME / 48));
         logFile.write("\n");
-        logFile.write("在办公区的人数：" + OFFICEING_AMOUNT);
+        logFile.write("Office：" + OFFICE_AMOUNT);
         logFile.write("\n");
-        logFile.write("在会议室的人数：" + MEETTING_AMOUNT);
+        logFile.write("Meeting Room：" + MEETING_AMOUNT);
         logFile.write("\n");
-        logFile.write("在茶水间的人数：" + RESTING_AMOUNT);
+        logFile.write("Rest Room：" + RESTING_AMOUNT);
         logFile.write("\n");
-        logFile.write("在洗手间的人数：" + TOILETING_AMOUNT);
+        logFile.write("Toilet：" + TOILETING_AMOUNT);
         logFile.write("\n");
-        logFile.write("被感染的人数：" + INFECTED_AMOUNT);
+        logFile.write("INFECTED：" + INFECTED_AMOUNT);
         logFile.write("\n");
-        logFile.write("存在风险的人数：" + RISKY_AMOUNT);
+        logFile.write("RISKY：" + RISKY_AMOUNT);
         logFile.write("\n");
         logFile.write("---------");
         logFile.flush();
     }
 
-    public void rework() throws IOException {
-        // 随机指定某员工为感染者
+    private int checkAll() throws IOException {
+        logFile.write("---Begin Daily Check---");
+        logFile.write("\n");
+        logFile.flush();
+        AtomicInteger ret = new AtomicInteger();
+        company.getEmployeePool().forEach(employee -> {
+            if (employee.getHealthyStatus() == HEALTHY_STATUS.ISOLATED)
+                return;
+            double d = Math.random();
+            if (employee.getHealthyStatus() == HEALTHY_STATUS.INFECTED && d < FIND_OUT_POSSIBILITY) {
+                try {
+                    logFile.write(employee.getNumber() + " Employee is infected");
+                    logFile.write("\n");
+                    logFile.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                employee.setHealthyStatus(HEALTHY_STATUS.ISOLATED);
+            }
+            if (employee.getHealthyStatus() == HEALTHY_STATUS.HEALTHY) {
+                ret.getAndIncrement();
+            }
+        });
+        logFile.write("---End Check---");
+        logFile.write("\n");
+        logFile.flush();
+        return ret.get();
+    }
+
+    synchronized private void tryReleaseRoom(WorkTask task) throws IOException {
+        boolean needRelease = false;
+        switch (task.getEmployee().getWorkStatus()) {
+            case WORKING:
+                break;
+            case MEETING:
+                if (task.getActionRoom().getUseDuration() > MEETING_DURATION) {
+                    needRelease = true;
+                }
+                break;
+            case RESTING:
+                if (task.getActionRoom().getUseDuration() > REST_DURATION) {
+                    needRelease = true;
+                }
+                break;
+            case PEEING:
+                if (task.getActionRoom().getUseDuration() > PEE_DURATION) {
+                    needRelease = true;
+                }
+                break;
+            case LIFTING:
+                if (task.getActionRoom().getUseDuration() > LIFT_DURATION) {
+                    needRelease = true;
+                }
+                break;
+            default:
+                break;
+        }
+        if (needRelease) {
+            // 房间复位
+            task.getActionRoom().setUseDuration(0);
+            task.getActionRoom().setUseStatus(STATUS.FREE);
+            // 房间里的人复位
+            String employeeNum = "";
+            List<Employee> list = task.getActionRoom().getEmployeeList();
+            try {
+                for (int i = 0; i < list.size(); i++) {
+                    list.get(i).setWorkStatus(WORK_STATUS.WORKING);
+                    employeeNum = employeeNum + list.get(i).getNumber() + ",";
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (!"".equals(employeeNum)) {
+                logFile.write(employeeNum + "退出" + task.getActionRoom().getNumber() + "回到了工位");
+                logFile.write("\n");
+                logFile.flush();
+            }
+            task.getActionRoom().getEmployeeList().clear();
+            // task复位
+            // task.setActionRoom(company.getFloorList()
+            // .get(Integer.parseInt(task.getEmployee().getNumber().split("-")[0])).getOffice());
+            task.getEmployee().setWorkStatus(WORK_STATUS.WORKING);
+        }
+        task.getActionRoom().setUseDuration(task.getActionRoom().getUseDuration() + 1);
+    }
+
+    private Room switchStatus(Employee e, Room room) {
+        if (e.getWorkStatus() == WORK_STATUS.WORKING) {
+
+            WORK_STATUS nextStatus = WORK_STATUS.WORKING;
+            double d = Math.random();
+            if (d <= MEETING_POSSIBILITY) {
+                nextStatus = WORK_STATUS.MEETING;
+            } else if (d > MEETING_POSSIBILITY && d <= (MEETING_POSSIBILITY + RESTING_POSSIBILITY)) {
+                nextStatus = WORK_STATUS.RESTING;
+            } else if (d > (MEETING_POSSIBILITY + RESTING_POSSIBILITY)
+                    && d <= (MEETING_POSSIBILITY + RESTING_POSSIBILITY + PEEING_POSSIBILITY)) {
+                nextStatus = WORK_STATUS.PEEING;
+            } else if (d > (MEETING_POSSIBILITY + RESTING_POSSIBILITY + PEEING_POSSIBILITY)
+                    && d <= (MEETING_POSSIBILITY + RESTING_POSSIBILITY + PEEING_POSSIBILITY + LIFTING_POSSIBILITY)) {
+                nextStatus = WORK_STATUS.LIFTING;
+            }
+            Floor floor = company.getFloorList().get(e.getNumber());
+
+            switch (nextStatus) {
+                case MEETING:
+                    MeetingRoom freeMeetingRoom = floor.getFreeMeetingRoom();
+                    if (freeMeetingRoom == null) {
+                        freeMeetingRoom = company.getFreeMeetingRoomPool();
+                        if (freeMeetingRoom == null) {
+
+                            return company.getFloorList().get(e.getNumber()).getOffice();
+                        }
+                    }
+                    freeMeetingRoom.join(e);
+                    return freeMeetingRoom;
+                case RESTING:
+                    RestRoom freeRestroom = floor.getFreeRestRoom();
+                    if (freeRestroom == null) {
+                        freeRestroom = company.getFreeRestroomPool();
+                        if (freeRestroom == null) {
+                            return company.getFloorList().get(e.getNumber()).getOffice();
+                        }
+                    }
+                    freeRestroom.join(e);
+                    return freeRestroom;
+                case PEEING:
+                    Toilet freeToilet = floor.getFreeToilet();
+                    if (freeToilet == null) {
+                        freeToilet = company.getFreeToiletPool();
+                        if (freeToilet == null) {
+                            return company.getFloorList().get(e.getNumber()).getOffice();
+                        }
+                    }
+                    freeToilet.join(e);
+                    return freeToilet;
+                case LIFTING:
+                    Elevator freeElevator = company.getFreeElevator();
+                    if (freeElevator == null) {
+                        return company.getFloorList().get(e.getNumber()).getOffice();
+                    }
+                    freeElevator.join(e);
+                    return freeElevator;
+                default:
+                    return company.getFloorList().get(e.getNumber()).getOffice();
+            }
+        } else if (e.getWorkStatus() == WORK_STATUS.HOMING) {
+
+            e.setWorkStatus(WORK_STATUS.WORKING);
+            return company.getFloorList().get(e.getNumber()).getOffice();
+        } else {
+
+            return room;
+        }
+    }
+
+    public void rework(int begin) throws IOException {
+        // randomly pick some employees to be the roots
         Random random = new Random();
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < begin; i++) {
             int index = random.nextInt(company.getEmployeePool().size());
             Employee root = company.getEmployeePool().get(index);
+            while (root.getHealthyStatus() == HEALTHY_STATUS.INFECTED) {
+                index = random.nextInt(company.getEmployeePool().size());
+                root = company.getEmployeePool().get(index);
+            }
             root.setHealthyStatus(HEALTHY_STATUS.INFECTED);
-            logFile.write("感染源为：" + root.getNumber() + "号员工");
+            logFile.write("The root is：" + root.getNumber() + " Employee");
             logFile.write("\n");
             logFile.flush();
         }
-        // 全员入队
+
         company.getFloorList().forEach(f -> {
             f.getEmployeeList().forEach(e -> {
                 e.setWorkStatus(WORK_STATUS.WORKING);
                 queue.add(new WorkTask(e, f.getOffice()));
             });
         });
-        logFile.write("开始复工");
+        logFile.write("Begin to rework");
         logFile.write("\n");
         logFile.flush();
-        // 状态计算
+
         long taskAmount = 0;
         while (queue.size() > 0) {
             WorkTask task = null;
